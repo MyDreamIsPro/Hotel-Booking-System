89c8ada4637a849153c2e103c315511f20989cec
Bui Huynh Duc <ducbh62workspace@gmail.com>
2022-05-29 21:40:48 +0700

    - Fixed null error in dashboard

--- server/src/controllers/dashboard.js
+++ server/src/controllers/dashboard.js
@@ -65,6 +65,14 @@ export const getDashboard = async (req, res) => {
         },
       },
     ]);
+    if (roomCount.length === 0) {
+      roomCount[0] = {
+        total_count: 0,
+        empty_count: 0,
+        pending_count: 0,
+        rented_count: 0,
+      };
+    }
     // BOOKING
     const bookingCount = await Booking.aggregate([
       {
@@ -84,6 +92,13 @@ export const getDashboard = async (req, res) => {
         },
       },
     ]);
+    if (bookingCount.length === 0) {
+      bookingCount[0] = {
+        total_count: 0,
+        check_in_count: 0,
+        check_out_count: 0,
+      };
+    }
     // INCOME EXPENSE
     const income = await Booking.aggregate([
       { $match: { status: { $gt: INTEGER.BOOKING_CANCELED } } },
